90dc8d5ba3fb298528b9f4f90ce32d5dd9d6e1c0
James Hawley <jamesrichardhawley@gmail.com>
2019-02-19 11:34:54 -0500

    Numerical sorting of chromosomes
    Updated consensus-generating script and results

--- Results/2018-11-28_global-accessbility/generate-consensus.sh
+++ Results/2018-11-28_global-accessbility/generate-consensus.sh
@@ -32,8 +32,8 @@ cat Consensus/{Ctrl,MB6,MB6Pen}.filtered.bed | sort -k1,1 -k2,2n | bedtools merg
 
 # generate consensus for all 3 conditions from the intersections
 echo "Generating 2-condition consensuses"
-cat Consensus/{Ctrl,MB6}.filtered.bed | sort -k1,1 -k2,2n | bedtools merge -i stdin > Consensus/consensus.Ctrl-MB6.bed
-cat Consensus/{Ctrl,MB6Pen}.filtered.bed | sort -k1,1 -k2,2n | bedtools merge -i stdin > Consensus/consensus.Ctrl-MB6Pen.bed
-cat Consensus/{MB6,MB6Pen}.filtered.bed | sort -k1,1 -k2,2n | bedtools merge -i stdin > Consensus/consensus.MB6-MB6Pen.bed
+cat Consensus/{Ctrl,MB6}.filtered.bed | sort -k1,1 -V -k2,2n | bedtools merge -i stdin > Consensus/consensus.Ctrl-MB6.bed
+cat Consensus/{Ctrl,MB6Pen}.filtered.bed | sort -k1,1 -V -k2,2n | bedtools merge -i stdin > Consensus/consensus.Ctrl-MB6Pen.bed
+cat Consensus/{MB6,MB6Pen}.filtered.bed | sort -k1,1 -V -k2,2n | bedtools merge -i stdin > Consensus/consensus.MB6-MB6Pen.bed
 
 echo "Done"
